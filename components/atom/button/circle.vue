d7e68cc7245bba4a2d4fe768c5c9fed7f5a09334
IIHARA HODAKA <iihara.h@xscore.co.jp>
2022-11-15 15:36:47 +0900

    [fix]　ユーザー一覧ページ実装

---
+++ components/atom/button/circle.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    btnClass: string
+  }>(),
+  {
+    btnClass: ''
+  }
+)
+</script>
+<template>
+  <v-hover v-slot="{ isHovering, props }">
+    <div
+      class="height-40 width-40 transition-medium-ease rounded-circle cursor-pointer"
+      :class="[
+        isHovering
+          ? 'elevation-3 background-white'
+          : 'background-grey-lighten-4',
+        btnClass
+      ]"
+      v-bind="props"
+      @click="$emit('btn-click')"
+    >
+      <slot />
+    </div>
+  </v-hover>
+</template>
--- pages/member/index.vue
+++ pages/member/index.vue
@@ -13,33 +13,54 @@ getUsers()
 </script>
 <template>
   <layout-public>
-    <atom-text font-size="text-h4" text="User" class="py-10" />
-    <div class="d-flex">
+    <atom-text font-size="text-h4" text="Member" class="py-10" />
+    <div class="d-flex flex-wrap" style="gap: 60px 5%">
       <v-sheet
         v-for="item in users"
         :key="item.id"
-        style="flex: 0 1 33.3%"
+        style="flex: 0 1 30%; padding: 0 3%"
         class="bg-transparent"
       >
-        <v-card class="w-100 elevation-5 rounded-lg">
-          <v-img src="sample.jpg" :aspect-ratio="3 / 13" cover />
-        </v-card>
-        <div
-          class="d-flex flex-nowrap justify-end bg-transparent my-3"
-          style="gap: 0 10px"
-        >
-          <atom-text
-            font-size="text-caption"
-            :text="$getYMD(item.createdAt)"
-            font-weight="font-weight-regular"
-          />
-          <atom-text
-            font-size="text-caption"
-            :text="$getYMD(item.updatedAt)"
-            font-weight="font-weight-regular"
-          />
+        <v-hover v-slot="{ isHovering, props }">
+          <v-card
+            class="w-100 elevation-5 rounded-circle transition-medium-ease"
+            v-bind="props"
+            :style="{
+              transform: isHovering ? 'scale(1.05)' : 'scale(1.0)'
+            }"
+            @click="navigateTo('/member/' + item.id)"
+          >
+            <v-img src="sample.jpg" :aspect-ratio="1" cover />
+          </v-card>
+        </v-hover>
+        <atom-text
+          font-size="text-h5"
+          class="text-center my-3"
+          :text="item.name"
+        />
+        <atom-text class="text-center ma-3" :text="item.belongs" />
+        <div class="d-flex flex-nowrap justify-center" style="gap: 0 4px">
+          <atom-button-circle
+            @btn-click="navigateTo(item.github, { external: true })"
+          >
+            <v-icon class="ma-2">mdi-github</v-icon>
+          </atom-button-circle>
+          <atom-button-circle
+            @btn-click="navigateTo(item.twitter, { external: true })"
+          >
+            <v-icon class="ma-2">mdi-twitter</v-icon>
+          </atom-button-circle>
+          <atom-button-circle
+            @btn-click="navigateTo(item.qiita, { external: true })"
+          >
+            <v-img src="/qiita.png" class="width-24 height-24 ma-2" />
+          </atom-button-circle>
+          <atom-button-circle
+            @btn-click="navigateTo(item.zenn, { external: true })"
+          >
+            <v-img src="/zenn.svg" class="width-24 height-24 ma-2" />
+          </atom-button-circle>
         </div>
-        <atom-text font-size="text-h5" :text="item.name" />
       </v-sheet>
     </div>
   </layout-public>
