ff3ca7b5be12e3550eee3a4c32afea8907ef1bf0
IIHARA HODAKA <iihara.h@xscore.co.jp>
2022-11-15 18:28:49 +0900

    [fix] パンくずコンポーネントの汎用化

--- components/atom/breadcrumbs/index.vue
+++ components/atom/breadcrumbs/index.vue
@@ -1,18 +1,35 @@
 <script setup lang="ts">
+type Breadcrumbs = {
+  title: string
+  to: string
+  disabled: boolean
+}
+const props = withDefaults(
+  defineProps<{
+    items: Breadcrumbs[]
+  }>(),
+  {
+    items: () => []
+  }
+)
 const { path } = useRoute()
-const breadcrumbs = [
-  { title: 'top', to: '/', disabled: path === '/' },
-  ...path
-    .slice(1)
-    .split('/')
-    .map((v) => {
-      return {
-        title: v,
-        to: '/' + v,
-        disabled: path.endsWith(v)
-      }
-    })
-]
+const breadcrumbs = computed(() => {
+  return props.items.length
+    ? [{ title: 'top', to: '/', disabled: path === '/' }, ...props.items]
+    : [
+        { title: 'top', to: '/', disabled: path === '/' },
+        ...path
+          .slice(1)
+          .split('/')
+          .map((v) => {
+            return {
+              title: v,
+              to: '/' + v,
+              disabled: path.endsWith(v)
+            }
+          })
+      ]
+})
 </script>
 <template>
   <v-breadcrumbs
