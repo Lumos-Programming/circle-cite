62ded13ccdcb967001d940287ecb640cde28aaa4
IIHARA HODAKA <iihara.h@xscore.co.jp>
2022-11-23 01:16:31 +0900

    [add] ページ追加

---
+++ pages/admin/link.vue
@@ -0,0 +1,117 @@
+<script setup lang="ts">
+import { Link, CreateLinkInput, ListLinksQuery } from '~/assets/API'
+import { createLink, deleteLink, updateLink } from '~/assets/graphql/mutations'
+import { listLinks } from '~/assets/graphql/queries'
+const { $listQuery, $baseMutation } = useNuxtApp()
+const links = ref<Link[]>([])
+const getLinks = async () => {
+  links.value = await $listQuery<ListLinksQuery, Link>({
+    name: 'listLinks',
+    query: listLinks
+  })
+}
+const filterAttr = (item: Link) => {
+  return {
+    id: item.id,
+    urls: item.urls || '',
+    likes: item.likes || 0
+  }
+}
+const input = ref<CreateLinkInput>({
+  urls: '',
+  likes: 0
+})
+const headers = [
+  { text: 'id', value: 'id' },
+  { text: 'urls', value: 'urls' },
+  { text: 'likes', value: 'likes' },
+  { text: 'oparation', value: 'oparation' }
+]
+getLinks()
+// TODO: valiidationを掛けること
+</script>
+<template>
+  <layout-admin>
+    <atom-text font-size="text-h4" text="Links" />
+    <atom-breadcrumbs class="mb-5" />
+    <v-card class="pa-5">
+      <div class="d-flex my-2">
+        <atom-text text="新規作成" font-size="text-h6" class="my-2" />
+        <v-spacer />
+        <atom-button
+          text="新規作成"
+          btn-class="border-solid border-width-1 border-grey-darken-4"
+          @btn-click="
+            $baseMutation({
+              name: 'createLink',
+              query: createLink,
+              input
+            })
+          "
+        />
+      </div>
+      <div v-for="item in Object.keys(input)" class="d-flex">
+        <atom-text
+          :text="item"
+          font-size="text-subtitle-2"
+          line-height="line-height-40"
+          style="flex: 0 0 120px"
+        />
+        <atom-input v-model="input[item]" :value="input[item]" :label="item" />
+      </div>
+    </v-card>
+    <v-card class="pa-5 my-5">
+      <div class="d-flex my-2">
+        <atom-text text="一括取得" font-size="text-h6" class="my-2" />
+        <v-spacer />
+        <atom-button
+          text="再取得"
+          btn-class="border-solid border-width-1 border-grey-darken-4"
+          @btn-click="getLinks()"
+        />
+      </div>
+      <easy-data-table
+        :headers="headers"
+        :items="links"
+        header-item-class-name="text-subtitle-2 font-weight-bold line-height-36"
+        body-row-class-name="height-48"
+        buttons-pagination
+        show-index
+      >
+        <template #expand="item">
+          <json-editor
+            v-model="links[item.index - 1]"
+            height="400"
+            mode="tree"
+          />
+        </template>
+        <template #item-oparation="item">
+          <div class="d-flex flex-nowrap">
+            <v-btn
+              icon="mdi-update"
+              variant="plain"
+              @click="
+                $baseMutation({
+                  name: 'updateLink',
+                  query: updateLink,
+                  input: filterAttr($findItem(links, 'id', item.id))
+                })
+              "
+            ></v-btn>
+            <v-btn
+              icon="mdi-delete"
+              variant="plain"
+              @click="
+                $baseMutation({
+                  name: 'deleteLink',
+                  query: deleteLink,
+                  input: { id: item.id }
+                })
+              "
+            ></v-btn>
+          </div>
+        </template>
+      </easy-data-table>
+    </v-card>
+  </layout-admin>
+</template>
