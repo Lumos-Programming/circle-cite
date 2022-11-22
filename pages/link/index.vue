a136e0faa8a64fdafc9367f7dca868e1ebe4c279
IIHARA HODAKA <iihara.h@xscore.co.jp>
2022-11-23 01:13:50 +0900

    [add] ページ追加

---
+++ pages/link/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Link, ListLinksQuery } from '~/assets/API'
+import { listLinks } from '~/assets/graphql/queries'
+const { $listQuery } = useNuxtApp()
+const links = ref<Link[]>([])
+const getLinks = async () => {
+  links.value = await $listQuery<ListLinksQuery, Link>({
+    name: 'listLinks',
+    query: listLinks
+  })
+}
+getLinks()
+</script>
+<template>
+  <layout-public>
+    <atom-text font-size="text-h4" text="Link" class="py-10" />
+  </layout-public>
+</template>
