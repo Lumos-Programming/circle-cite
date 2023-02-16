92953616256d33e0430a43fccea6b85f9ca53475
IIHARA HODAKA <iihara.h@xscore.co.jp>
2023-02-16 22:18:41 +0900

    [fix] refactor

--- components/layout/public.vue
+++ components/layout/public.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="bg-grey-color overflow-hidden designed-scrollbar" style="min-height: 100vh">
     <div
-      :style="{ maxWidth: '920px' }"
+      :style="{ maxWidth: '920px', minHeight: 'calc(var(--vh, 1vh) * 100 - 88px)' }"
       class="mx-5 mx-md-auto"
-      style="min-height: calc(var(--vh, 1vh) * 100 - 88px)"
     >
       <module-header />
       <slot />
