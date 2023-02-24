ed98c31e149bdda026f4d9d9b5fb7ad6c9f5cdd4
Shion Ichikawa <shion1305@gmail.com>
2023-02-24 23:50:54 +0900

    ✨ (plugins)v-calendar

---
+++ plugins/calendar.client.ts
@@ -0,0 +1,7 @@
+// @ts-ignore
+import { SetupCalendar } from 'v-calendar'
+import 'v-calendar/dist/style.css'
+
+export default defineNuxtPlugin((nuxtApp) => {
+    nuxtApp.vueApp.use(SetupCalendar, {})
+})
