1229c3568f8e49e242c379e8afe3716cbcbe3969
IIHARA HODAKA <iihara.h@xscore.co.jp>
2023-03-15 09:34:37 +0900

    [fix] fix design

--- pages/article/[id].vue
+++ pages/article/[id].vue
@@ -62,7 +62,7 @@ await fetchArticle()
           </v-chip>
         </v-hover>
       </v-chip-group>
-      <div class="mt-10">{{ article.body }}</div>
+      <div class="mt-10 ProseMirror">{{ article.body }}</div>
     </div>
   </layout-public>
 </template>
